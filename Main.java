a9f4d10c4c252244de618df6ca30ab60490d1668
dawids12 <101203650+dawids12@users.noreply.github.com>
2022-04-05 08:53:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int pom1 = 1;
+    for(int i = 0; i<5;i++)
+      {
+        for(int y =0; y<pom1; y++ )
+          {
+            System.out.print("*");
+          }
+          pom1 = pom1+1;
+          System.out.println(" ");
+      }
+  }
+}
